e0d6edc151de8e0dfecf1d58ddac03a0c4805718
Sebastian Krupke <sebastian.krupke@ewe.de>
2023-04-14 14:05:06 +0200

    First setup of Serverless stack with CodeWhisperer

--- README.md
+++ README.md
@@ -8,10 +8,28 @@ This is a basic serverless application that was developed as an experiment for u
 Setup of the Serverless CDK App with projen.
 
 ### What AWS CodeWhisperer did?
-* 
+* Could not support in this case
 
 ### What did we do?
 * ```npx projen new awscdk-app-ts```
 
 ### Problems
-* 
+* None
+
+# Creation of the Serverless CDK Stack
+Programming of the Serverless Stack with AWS CDK.
+
+### What AWS CodeWhisperer did?
+* Gave line-based hints for AWS CDK components and their configuration
+* Provided good hints for names, e.g. "customerTable"
+* Gave hints that show contextual knowledge without typing, e.g.:
+  * ```customerTable.grantReadWriteData(importCustomerFunction);```
+
+### What did we do?
+* Orchestrating of components
+* Fix errors that may result from imports/ other CDK versions
+
+### Problems
+* Some hints were pointless or doesn't match
+* Only one-liner suggestions, not whole AWS CDK components
+* Errors (maybe methods from old CDK versions were suggested)
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,52 @@
-import { App, Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import {App, RemovalPolicy, Stack, StackProps} from 'aws-cdk-lib';
+import {Construct} from 'constructs';
+import {LambdaIntegration, RestApi} from "aws-cdk-lib/aws-apigateway";
+import {Table} from "aws-cdk-lib/aws-dynamodb";
+import {Code, Function, Runtime} from "aws-cdk-lib/aws-lambda";
+
+/**
+ * WARNING: Errors in this code are intended as they may show actual capabilities of AWS CodeWhisperer.
+ */
 
 export class MyStack extends Stack {
   constructor(scope: Construct, id: string, props: StackProps = {}) {
     super(scope, id, props);
 
-    // define resources here...
+    const customerS3Bucket = new Table(this, 'CustomerS3Bucket', {
+      tableName: 'CustomerS3Bucket',
+    });
+
+    const customerTable = new Table(this, 'CustomerTable', {
+      tableName: 'CustomerTable',
+      partitionKey: {
+        name: 'customerId',
+        type: Table.AttributeType.STRING
+      },
+      billingMode: Table.BillingMode.PAY_PER_REQUEST,
+      removalPolicy: RemovalPolicy.DESTROY,
+    });
+
+    const customerRestApi = new RestApi(this, 'CustomerRestApi', {
+      restApiName: 'Customer Service',
+      description: 'This service serves customers',
+      deployOptions: {
+        stageName: 'dev',
+      },
+    });
+
+    // CDK Nodejs Lambda function
+    const importCustomerFunction =  new Function(this, 'ImportCustomerFunction', {
+      runtime: Runtime.NODEJS_14_X,
+      code: Code.fromAsset('src'),
+      handler: 'importCustomer.handler',
+      environment: {
+        TABLE_NAME: 'Customer'
+      }
+    });
+
+    customerS3Bucket.grantReadData(importCustomerFunction);
+    customerTable.grantReadWriteData(importCustomerFunction);
+
   }
 }
 
